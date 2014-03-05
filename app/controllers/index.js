fc9b91cae5df169b6fd6d297f405b448eb68294c
elbuo8 <yamil.asusta@sendgrid.com>
2014-03-05 16:20:14 -0400

    First release

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,27 @@
 function doClick(e) {
-	var api_user="<youruser>";
-	var api_key="<yourpassword>";
+	var api_user='<youruser>';
+	var api_key='<yourpassword>';
 
 	var sendgrid  = require('tisendgrid')(api_user, api_key);
 
-	var cb_onsuccess=function(e){
-		console.log('Success: ' + e)
-	}
+	var cb_onsuccess = function(e) {
+		console.log('Success: ' + e);
+	};
 
-	var cb_onerror=function(err,status,body){
+	var cb_onerror = function(err, status, body) {
 		console.log('Error: ' + err + '; Status: ' + status + '; Body: ' + body);
-	}
+	};
 
-	var email={
-	  		to:       	'to@email.com',
-	  		toname: 	'to name',
-	  		from:  	   	'from@email.com',
-	  		fromname: 	'from name',
-	  		subject:  	'Hello World',
-	  		text:     	'My first email through SendGrid.',
-	  		html: 	  	'<b>This is a test!!!</b>'
-		}
+	var email = {
+	  to:       	['john@doe.com', 'doe@emai.com'],
+	  from:  	   'yamil@aloha.com',
+	  subject:  	'AppC Test',
+	  text:     	'My first email through SendGrid.'
+	};
 
+	var message = sendgrid.Email(email);
 
-    /*testFileObj = Titanium.Filesystem.getFile('appicon.png');
-    testFileBin = f1.read();
-    email.image=testFileBin;*/
-
-	sendgrid.send(email,cb_onsuccess,cb_onerror);
+	sendgrid.send(message,cb_onsuccess,cb_onerror);
 }
 
 $.index.open();
--- app/lib/tisendgrid.js
+++ app/lib/tisendgrid.js
@@ -1,50 +1,262 @@
-/*
-* tiSendgrid : a Sendgrid module for Appcelerator Titanium
-* Author: Ricardo Alcocer (@ricardoalcocer)
-*
-* Based on https://github.com/sendgrid/sendgrid-nodejs
-*
-*/
-
 "use strict";
 
-var Sendgrid = function(api_user, api_key) {
-  if( !(this instanceof Sendgrid) ) {
-    return new Sendgrid(api_user, api_key);
+function smtpapi(header) {
+  header = header || {};
+  this.header = {};
+  this.header.to = header.to || [];
+  this.header.sub = header.sub || {};
+  this.header.unique_args = header.unique_args || {};
+  this.header.category = header.category || [];
+  this.header.section = header.section || {};
+  this.header.filters = header.filters || {};
+}
+
+smtpapi.prototype.addTo = function(to) {
+  if (to instanceof Array) {
+    this.header.to = this.header.to.concat(to);
+  } else {
+    this.header.to.push(to);
+  }
+};
+
+smtpapi.prototype.setTos = function(to) {
+  if (to instanceof Array) {
+    this.header.to = to;
+  } else {
+    this.header.to = [to];
+  }
+};
+
+smtpapi.prototype.addSubstitution = function(key, val) {
+  if (this.header.sub[key] === undefined) this.header.sub[key] = [];
+  if (val instanceof Array) {
+    this.header.sub = this.header.sub[key].concat(val);
+  } else {
+    this.header.sub[key].push(val);
+  }
+};
+
+smtpapi.prototype.setSubstitutions = function(subs) {
+  this.header.sub = subs;
+};
+
+smtpapi.prototype.addUniqueArg = function(key, val) {
+  this.header.unique_args[key] = val;
+};
+
+smtpapi.prototype.setUniqueArgs = function(val) {
+  this.header.unique_args = val;
+};
+
+smtpapi.prototype.addCategory = function(cat) {
+  if (cat instanceof Array) {
+    this.header.category.concat(cat);
+  } else {
+    this.header.category.push(cat);
+  }
+};
+
+smtpapi.prototype.setCategories = function(cats) {
+  if (cats instanceof Array) {
+    this.header.category = cats;
+  } else {
+    this.header.category = [cats];
+  }
+};
+
+smtpapi.prototype.addSection = function(sec, val) {
+  this.header.section[sec] = val;
+};
+
+smtpapi.prototype.setSections = function(sec) {
+  this.header.section = sec;
+};
+
+smtpapi.prototype.addFilter = function(filter, setting, val) {
+  if (this.header.filters[filter] === undefined) {
+    this.header.filters[filter] = {'settings': {}};
+  }
+  this.header.filters[filter]['settings'][setting] = val;
+};
+
+smtpapi.prototype.setFilters = function(filters) {
+  this.header.filters = filters;
+};
+
+smtpapi.prototype.jsonString = function() {
+  var header = {};
+  for (var key in this.header) {
+    if (this.header.hasOwnProperty(key) && Object.keys(this.header[key]).length) {
+      header[key] = this.header[key];
+    }
+  }
+  return JSON.stringify(header);
+};
+
+function Email(mail) {
+  if(!(this instanceof Email)) {
+    return new Email(mail);
   }
 
-  var send = function(email,onsuccess,onerror) {
-    email.api_user=api_user;
-    email.api_key=api_key;
 
-    var postOptions = {
-      method  : 'POST',
-      uri     : 'https://api.sendgrid.com/api/mail.send.json'
-    };
+  mail = mail || {};
+  smtpapi.call(this, mail['x-smtpapi'])
+  this.body = {};
+  this.toCounter = 0;
+  this.bccCounter = 0;
+  this.tonameCounter = 0;
+  this.addTo(mail['to'] || mail['to[]']);
+  this.addToName(mail['toname'] || mail['toname[]']);
+  this.body['from'] = mail.from || '';
+  this.body['fromname'] = mail.fromname || '';
+  this.body['subject'] = mail.subject || '';
+  this.body['text'] = mail.text || '';
+  this.body['html'] = mail.html || '';
+  this.addBcc(mail['bcc'] || mail['bcc[]'])
+  this.body['replyto'] = mail.replyto || '';
+  this.body['date'] = mail.date || new Date().toUTCString();
+  this.body['headers'] = mail.headers || '';
+  if (mail.files && mail instanceof Array) {
+    for (var i = 0, len = mail.files.length; i < len; i++) {
+      this.addFile(mail.files[i].file, mail.files[i].name);
+    }
+  }
+}
 
+Email.prototype = Object.create(smtpapi.prototype);
+Email.prototype.constructor = Email;
+
+Email.prototype.addTo = function(email) {
+  if (email instanceof Array) {
+    for(var i = 0, len = email.length; i < len; i++) {
+      this.body['to[' + this.toCounter++ +']'] = email[i];
+    }
+  } else {
+    this.body['to['+ this.toCounter++ +']'] = email;
+  }
+};
+
+Email.prototype.addToName = function(name) {
+  if (name instanceof Array) {
+    for(var i = 0, len = name.length; i < len; i++) {
+      this.body['toname[' + this.tonameCounter++ +']'] = name[i];
+    }
+  } else {
+    this.body['toname['+ this.tonameCounter++ +']'] = name;
+  }
+};
+
+Email.prototype.setFromName = function(name) {
+  this.body['fromname'] = name;
+};
+
+Email.prototype.setSubject = function(subject) {
+  this.body['subject'] = subject;
+};
+
+Email.prototype.setText = function(text) {
+  this.body['text'] = text;
+};
+
+Email.prototype.setHTML = function(html) {
+  this.body['html'] = html;
+};
+
+Email.prototype.addBcc = function(bcc) {
+  if (bcc instanceof Array) {
+    for(var i = 0, len = bcc.length; i < len; i++) {
+      this.body['bcc[' + this.bccCounter++ + ']'] = bcc[i];
+    }
+  } else {
+    this.body['bcc['+ this.bccCounter++ +']'] = bcc;
+  }
+};
+
+Email.prototype.setReplyTo = function(replyto) {
+  this.body['replyto'] = replyto;
+};
+
+Email.prototype.setDate = function(date) {
+  this.body['date'] = date;
+};
+
+Email.prototype.addFile = function(file, name) {
+  if (typeof file === 'string') {
+    file = Titanium.Filesystem.getFile(file);
+  }
+  name = name || file.name;
+  this.body['files[' + name + ']'] = file;
+};
+
+Email.prototype.setHeader = function(header) {
+  this.body['header'] = header;
+};
+
+Email.prototype.setAPIHeader = function(header) {
+  this.body['x-smtpapi'] = header || this.jsonString();
+};
+
+Email.prototype.getEmail = function() {
+  this.setAPIHeader();
+  var body = {};
+  for (var key in this.body) {
+    if (this.body.hasOwnProperty(key) && this.body[key]) {
+      if (this.body[key] instanceof Array && this.body[key].length === 0) {
+        continue;
+      }
+      body[key] = this.body[key];
+    }
+  }
+  return body;
+};
+
+function SendGrid(api_user, api_key) {
+  if(!(this instanceof SendGrid)) {
+    return new SendGrid(api_user, api_key);
+  }
+
+  //Private
+  var _buildBody = function(mail) {
+    if (mail.constructor.name === 'Email') {
+      mail = mail.getEmail();
+    } else {
+      mail = (new Email(mail)).getEmail();
+    }
+    mail['api_user'] = credentials.api_user;
+    mail['api_key'] = credentials.api_key;
+    return mail;
+  };
+
+  var options = {
+    method  : 'POST',
+    uri     : 'https://api.sendgrid.com/api/mail.send.json'
+  };
+
+  var credentials = {
+    api_user: api_user,
+    api_key: api_key
+  };
+
+  //Priviledged
+  this.send = function (email, success, error) {
     var client = Ti.Network.createHTTPClient({
-      onload : function(e) {
-        onsuccess(this.responseText);
+      onload: function(e) {
+        success(this.responseText);
       },
-      onerror : function(e) {
-        onerror(this.error,this.status,this.responseText);
+      onerror: function(e) {
+        error(this.error, this.status, this.responseText);
       },
-      timeout : 5000
-    })
-      
-    client.open(postOptions.method, postOptions.uri);
+      timeout: 5000
+    });
+
+    var email = _buildBody(email);
+    client.open(options.method, options.uri);
     client.send(email);
   };
 
-  /*
-  * Expose public API calls
-  */
+  this.Email = Email;
+  this.smtpapi = smtpapi;
 
-  this.api_user = api_user;
-  this.api_key = api_key;
-  this.send = send;
+}
 
-  return this;
-};
-
-module.exports = Sendgrid;
+module.exports = SendGrid;
